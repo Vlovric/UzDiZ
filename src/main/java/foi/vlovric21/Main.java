c6c590ec88efc57ab34e2b051964a5750bae2cbb
Viktor Lovric <vlovric21@student.foi.hr>
2025-10-28 18:27:23 +0100

    Napravljeno parsiranje za aranzman csv

--- src/main/java/foi/vlovric21/Main.java
+++ src/main/java/foi/vlovric21/Main.java
@@ -1,6 +1,8 @@
 package foi.vlovric21;
 
 import foi.vlovric21.parser.ArgumentParser;
+import foi.vlovric21.parser.CsvParser;
+import foi.vlovric21.parser.CsvTip;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,7 +18,8 @@ public class Main {
     }
 
     static void ucitajPodatke(String aranzmaniDatoteka, String rezervacijeDatoteka){
-
+        CsvParser csvParser = new CsvParser();
+        csvParser.parsirajCsv(aranzmaniDatoteka, CsvTip.ARANZMAN);
     }
     static void interaktivniNacinRada(){
 
--- src/main/java/foi/vlovric21/parser/CsvParser.java
+++ src/main/java/foi/vlovric21/parser/CsvParser.java
@@ -3,6 +3,7 @@ package foi.vlovric21.parser;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -31,6 +32,8 @@ public class CsvParser {
             "Oznaka aranžmana",
             "Datum i  vrijeme"
     );
+    private static final List<Integer> obaveznaZaglavljaAranzman = List.of(0,1,2,3,4,7,8,9,10);
+    private static final List<Integer> obaveznaZaglavljaRezervacija = List.of(0,1,2,3);
 
     public boolean validirajZaglavlje(List<String> ocekivanoZaglavlje, String redZaglavlja){
         List<String> dobivenoZaglavlje = Arrays.stream(redZaglavlja.split(","))
@@ -41,10 +44,77 @@ public class CsvParser {
     }
 
     public List<String> parsirajRed(String red){
+        List<String> polja = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        boolean unutarNavodnika = false;
 
+        for(int i=0; i<red.length(); i++){
+            char znak = red.charAt(i);
+
+            if(znak == '"'){
+                unutarNavodnika = !unutarNavodnika;
+            }else if(znak == ',' && !unutarNavodnika){
+                polja.add(sb.toString().trim());
+                sb.setLength(0);
+            }else{
+                sb.append(znak);
+            }
+        }
+
+        polja.add(sb.toString().trim());
+        return polja;
     }
 
     private boolean validirajRed(List<String> red, CsvTip tip){
+        String regexDatum = "\\d{1,2}\\.\\d{1,2}\\.\\d{4}\\.?";
+        String regexDatumVrijeme = "\\d{1,2}\\.\\d{1,2}\\.\\d{4}\\.? \\d{1,2}:\\d{2}";
+        if(tip == CsvTip.ARANZMAN){
+            if(red.size() != zaglavljeAranzman.size()){
+                System.out.println("Red nema polja koliko zaglavlje stupaca"); //TODO uklonit ispis
+                return false;
+            }
+            for(int index : obaveznaZaglavljaAranzman){
+                if(red.get(index).isEmpty()){
+                    System.out.println("Obavezno polje je prazno, index: " + index); //TODO uklonit ispis
+                    return false;
+                }
+            }
+            try { //TODO uvest za opcionalne validaciju i refaktorirat
+                Integer.parseInt(red.get(0)); //Oznaka
+                Integer.parseInt(red.get(7)); //Cijena
+                Integer.parseInt(red.get(8)); //Min broj putnika
+                Integer.parseInt(red.get(9)); //Maks broj putnika
+                Integer.parseInt(red.get(10)); //Broj noćenja
+            }catch(NumberFormatException ex){
+                System.out.println("Neispravan broj u jednom od obaveznih polja"); //TODO uklonit ispis
+                return false;
+            }
+            if(!red.get(3).matches(regexDatum) || !red.get(4).matches(regexDatum)){
+                System.out.println("Neispravan format datuma u jednom od obaveznih polja"); //TODO uklonit ispis
+                return false;
+            }
+        }else{
+            if(red.size() != zaglavljeRezervacija.size()){
+                System.out.println("Red nema polja koliko zaglavlje stupaca"); //TODO uklonit ispis
+                return false;
+            }
+            for(int index : obaveznaZaglavljaRezervacija){
+                if(red.get(index).isEmpty()){
+                    System.out.println("Obavezno polje je prazno, index: " + index); //TODO uklonit ispis
+                    return false;
+                }
+            }
+            try{
+                Integer.parseInt(red.get(2)); //Oznaka aranžmana
+            }catch(NumberFormatException ex){
+                System.out.println("Neispravan broj polja oznake"); //TODO uklonit ispis
+                return false;
+            }
+            if(!red.get(3).matches(regexDatumVrijeme)){
+                System.out.println("Neispravan format datuma"); //TODO uklonit ispis
+                return false;
+            }
+        }
         return true;
     }
 
@@ -73,11 +143,23 @@ public class CsvParser {
                     continue;
                 }
                 List<String> polja = parsirajRed(red);
-
+                if(!validirajRed(polja, tip)){
+                    System.out.println("Neispravan red " + redniBroj + " u datoteci: " + datoteka);
+                    continue;
+                }
+                //System.out.println("Redak " + redniBroj + ":" + "\n");
+                /*
+                for(int i=0; i<polja.size(); i++){
+                    System.out.println(zaglavlje.get(i) + ": " + polja.get(i));
+                    System.out.println("\n");
+                }
+                */
+                //kreirat objekt i dodat u Repozitorij
             }
         }catch(IOException ex){
             System.out.println("Greška kod čitanja datoteke: " + datoteka);
             return false;
         }
+        return true;
     }
 }
