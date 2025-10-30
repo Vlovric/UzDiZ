a1c9fe208a2908de9e6cbb59f520a90f8a965488
Viktor Lovric <vlovric21@student.foi.hr>
2025-10-30 13:56:20 +0100

    Napravljeno kompletno logganje i validacija radi

--- src/main/java/foi/vlovric21/Main.java
+++ src/main/java/foi/vlovric21/Main.java
@@ -20,7 +20,7 @@ public class Main {
         }
 
         ucitajPodatke(parser.getAranzmaniDatoteka(), parser.getRezervacijeDatoteka());
-        testirajObjekte();
+        //testirajObjekte();
         interaktivniNacinRada();
     }
 
@@ -36,7 +36,7 @@ public class Main {
         // stvorit ocekivane objekte za aranzmane
         AranzmanDirector director = new AranzmanDirector();
         List<Aranzman> ocekivaniAranzmani = List.of(
-                director.stvoriAranzman(1, "Tocno samo obavezno", "Program", "01.01.2025.", "02.01.2025.", 1, 1, 2, null, null, null, null, "", null, null, null),
+                director.stvoriAranzman(1, "Tocno samo obavezno", "Program", "01.01.2025.", "02.01.2025.", 1, 1, 2, null, "", "", null, "", null, null, null),
                 director.stvoriAranzman(2, "Tocno sve", "Program", "01.01.2025.", "02.01.2025.", 1, 1, 2, 1, "10:00", "11:00", 10, "avion; bus", 1, 1, 1),
                 director.stvoriAranzman(3, "Tocno sve svi brojevi 0", "Program", "01.01.2025.", "02.01.2025.", 0, 0, 0, 0, "10:00", "11:00", 0, "avion;bus", 0, 0, 0),
                 director.stvoriAranzman(18, "Tocno razmak cijena", "Program", "01.01.2025.", "02.01.2025.", 1, 1, 2, 1, "10:00", "11:00", null, "", null, null, null),
@@ -45,11 +45,11 @@ public class Main {
                 director.stvoriAranzman(21, "Tocno datum nema tocku i maknute nule", "Program", "1.01.2025", "02.1.2025", 1, 1, 2, 1, "10:00", "11:00", null, "", null, null, null),
                 director.stvoriAranzman(22, "Tocno sve bez razmaka", "Program", "01.01.2025.", "02.01.2025.", 1, 1, 2, 1, "10:00", "11:00", 10, "avion; bus", 1, 1, 1)
         );
-        // stvorit ocekivane objekte za rezervacije
-        // usporedit s pravima
         List<Aranzman> stvarniAranzmani = RepozitorijPodataka.getInstance().getAranzmani();
+        // stvorit ocekivane objekte za rezervacije
+
+        //usporedit i ispisat
         usporediAranzmane(ocekivaniAranzmani, stvarniAranzmani);
-        // ispisat rezultate
     }
 
     static void usporediAranzmane(List<Aranzman> ocekivani, List<Aranzman> stvarni) {
@@ -61,11 +61,80 @@ public class Main {
                 System.out.println("Nedostaje aranžman s oznakom: " + ocekivaniAranzman.getOznaka());
             } else if (!ocekivaniAranzman.equals(stvarniAranzman)) {
                 System.out.println("Aranžman s oznakom " + ocekivaniAranzman.getOznaka() + " se ne podudara.");
-                System.out.println("Očekivano: " + ocekivaniAranzman);
-                System.out.println("Stvarno: " + stvarniAranzman);
+                isprintajAranzman(ocekivaniAranzman, stvarniAranzman);
+                System.out.println();
             } else {
-                System.out.println("Aranžman s oznakom " + ocekivaniAranzman.getOznaka() + " je ispravan.");
+                System.out.println("Aranžman s oznakom " + ocekivaniAranzman.getOznaka() + " je ispravan.\n");
             }
         }
+    }
+
+    static void isprintajAranzman(Aranzman ocekivaniAranzman, Aranzman stvarniAranzman) {
+        System.out.println("Oznaka ocekivani: |" + ocekivaniAranzman.getOznaka() + "|");
+        System.out.println("Oznaka stvarni  : |" + stvarniAranzman.getOznaka() + "|");
+        System.out.println();
+
+        System.out.println("Naziv ocekivani: |" + ocekivaniAranzman.getNaziv() + "|");
+        System.out.println("Naziv stvarni  : |" + stvarniAranzman.getNaziv() + "|");
+        System.out.println();
+
+        System.out.println("Program ocekivani: |" + ocekivaniAranzman.getProgram() + "|");
+        System.out.println("Program stvarni  : |" + stvarniAranzman.getProgram() + "|");
+        System.out.println();
+
+        System.out.println("Pocetni datum ocekivani: |" + ocekivaniAranzman.getPocetniDatum() + "|");
+        System.out.println("Pocetni datum stvarni  : |" + stvarniAranzman.getPocetniDatum() + "|");
+        System.out.println();
+
+        System.out.println("Zavrsni datum ocekivani: |" + ocekivaniAranzman.getZavrsniDatum() + "|");
+        System.out.println("Zavrsni datum stvarni  : |" + stvarniAranzman.getZavrsniDatum() + "|");
+        System.out.println();
+
+        System.out.println("Cijena ocekivani: |" + ocekivaniAranzman.getCijena() + "|");
+        System.out.println("Cijena stvarni  : |" + stvarniAranzman.getCijena() + "|");
+        System.out.println();
+
+        System.out.println("Min broj putnika ocekivani: |" + ocekivaniAranzman.getMinBrojPutnika() + "|");
+        System.out.println("Min broj putnika stvarni  : |" + stvarniAranzman.getMinBrojPutnika() + "|");
+        System.out.println();
+
+        System.out.println("Maks broj putnika ocekivani: |" + ocekivaniAranzman.getMaxBrojPutnika() + "|");
+        System.out.println("Maks broj putnika stvarni  : |" + stvarniAranzman.getMaxBrojPutnika() + "|");
+        System.out.println();
+
+        System.out.println("Broj nocenja ocekivani: |" + ocekivaniAranzman.getBrojNocenja() + "|");
+        System.out.println("Broj nocenja stvarni  : |" + stvarniAranzman.getBrojNocenja() + "|");
+        System.out.println();
+
+        System.out.println("Vrijeme kretanja ocekivani: |" + ocekivaniAranzman.getVrijemeKretanja() + "|");
+        System.out.println("Vrijeme kretanja stvarni  : |" + stvarniAranzman.getVrijemeKretanja() + "|");
+        System.out.println();
+
+        System.out.println("Vrijeme povratka ocekivani: |" + ocekivaniAranzman.getVrijemePovratka() + "|");
+        System.out.println("Vrijeme povratka stvarni  : |" + stvarniAranzman.getVrijemePovratka() + "|");
+        System.out.println();
+
+        System.out.println("Doplata za jednokrevetnu sobu ocekivani: |" + ocekivaniAranzman.getDoplataZaJednokrevetnuSobu() + "|");
+        System.out.println("Doplata za jednokrevetnu sobu stvarni  : |" + stvarniAranzman.getDoplataZaJednokrevetnuSobu() + "|");
+        System.out.println();
+
+        System.out.println("Prijevoz ocekivani: |" + ocekivaniAranzman.getPrijevoz() + "|");
+        System.out.println("Prijevoz stvarni  : |" + stvarniAranzman.getPrijevoz() + "|");
+        System.out.println();
+
+        System.out.println("Broj dorucka ocekivani: |" + ocekivaniAranzman.getBrojDorucka() + "|");
+        System.out.println("Broj dorucka stvarni  : |" + stvarniAranzman.getBrojDorucka() + "|");
+        System.out.println();
+
+        System.out.println("Broj ruckova ocekivani: |" + ocekivaniAranzman.getBrojRuckova() + "|");
+        System.out.println("Broj ruckova stvarni  : |" + stvarniAranzman.getBrojRuckova() + "|");
+        System.out.println();
+
+        System.out.println("Broj vecera ocekivani: |" + ocekivaniAranzman.getBrojVecera() + "|");
+        System.out.println("Broj vecera stvarni  : |" + stvarniAranzman.getBrojVecera() + "|");
+        System.out.println();
+
+
+
     }
 }
